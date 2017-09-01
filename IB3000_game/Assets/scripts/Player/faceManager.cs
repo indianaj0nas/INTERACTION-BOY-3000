d2712037d2f1c86339d8cd91f300d5ffedd4da04
indianaj0nas <indianajonas42@gmail.com>
2017-09-01 17:46:51 +0200

    Test of 3D boy
    First test at setting up an animated face and textured 3D boy.

---
+++ IB3000_game/Assets/scripts/Player/faceManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class faceManager : MonoBehaviour {
+
+    public GameObject face;
+
+    public Transform faceBone;
+
+	void Awake ()
+    {
+        face.transform.parent = faceBone;
+        face.transform.position = faceBone.transform.position;
+    }
+}
